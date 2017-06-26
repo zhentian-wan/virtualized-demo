9b1e4fb1a57cbdb8c7df7f125d210b3e7eabb9cb
Zhentian Wan <answer881215@gmail.com>
2017-06-26 22:21:24 +0300

    CellMeasurer for random height

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import React, {Component} from 'react';
-import {AutoSizer, List} from 'react-virtualized';
+import {AutoSizer, List, CellMeasurer, CellMeasurerCache} from 'react-virtualized';
 
 const ScreenInfo = ({width, height}) => (<span>width: {width} height: {height}</span>);
 
 class App extends Component {
 
-    renderRow = ({key, isScrolling, style, index}) => {
+    constructor(props) {
+        super(props);
+        this.cache = new CellMeasurerCache({
+            fixedWidth: true,
+            defaultHeight: 50
+        });
+    }
+    renderRow = ({key, isScrolling, parent, style, index}) => {
         return (
-            <div style={style} key={key}>
+        <CellMeasurer
+            key={key}
+            cache={this.cache}
+            parent={parent}
+            columnIndex={0}
+            rowIndex={index}
+        >
+            <div style={style} >
                 name: {this.props.data[index].name}
                 email: {this.props.data[index].email}
+                height: <div style={{height: `${this.props.data[index].randomHeight}px`}}>{this.props.data[index].randomHeight}px</div>
             </div>
+        </CellMeasurer>
         );
     };
 
@@ -23,7 +39,8 @@ class App extends Component {
                             <ScreenInfo width={width} height={height}/>
                             <List
                                 rowCount={this.props.data.length}
-                                rowHeight={50}
+                                deferredMeasurementCache={this.cache}
+                                rowHeight={this.cache.rowHeight}
                                 rowRenderer={this.renderRow}
                                 width={width}
                                 height={height}
